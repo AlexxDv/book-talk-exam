b65190d64a9b86f47f9e9fd1ecb8c42ea7d6c946
AlexxDv <alexxdimov@gmail.com>
2023-02-12 17:39:24 +0100

    add Book model

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const {Schema, model, Types} = require('mongoose');
+
+const URL_PATTERN = /^https?:\/\/.+$/i;
+
+const bookShema = new Schema({
+    title: {type: String, required: true, minLength: [2, "Title length must be at least 2 characters long"]}, 
+    author: {type: String, required: true, minLength: [5, "Title length must be at least 5 characters long"]},
+    imageUrl: {type: String, required: true, validate: {
+        validator: (value) => URL_PATTERN.test(value),
+        message: 'Please enter a valid url'
+    }},
+    review: {type: String, required: true, minLength: [10, "Preview length must be at least 10 characters long"]},
+    genre: {type: String, required: true},
+    stars: {type: Number, required: true, validate: {
+        validator: function(value) {
+          return value >= 1 && value <= 5;
+        },
+        message: 'Stars must be between 1 and 5.'
+      }},
+    wishingLisg: { type: [Types.ObjectId], ref: User, default: []},
+    owner: {type: Types.ObjectId, ref: User, required: true},
+})
+
+const Book = model('Book', bookShemama);
+
+module.exports = Book;
